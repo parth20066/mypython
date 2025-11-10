3768465f696f12603e875c93e894bddc475b995b
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-10 16:31:30 +0530

    Create areaofcircle.py

---
+++ areaofcircle.py
@@ -0,0 +1,12 @@
+number1=int(input("enter number1 "))
+number2=int(input("enter number2 "))
+div1=number1/number2
+sub=number1-number2
+mul=number1*number2
+div2=number1//number2
+div3=number1%number2
+addition=number1+number2
+print(div1,div2,div3,sub,mul,addition,)
+enter number1 10
+enter number2 20
+0.5 0 10 -10 200 30
